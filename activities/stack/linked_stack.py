bd1fc5d5f79d0ab302921610e8e95f9492d6aa3b
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2026-05-01 11:26:28 -0700

    docs: Improve documentation

--- activities/stack/linked_stack.py
+++ activities/stack/linked_stack.py
@@ -1,4 +1,6 @@
-""""Implement a stack collection that supports `is_empty`, `push` and `pop`."""
+""""Implement a stack data collection that supports the `is_empty`, `push` and
+`pop` methods.
+"""
 
 class LinkedStack:
 
@@ -8,13 +10,17 @@ class LinkedStack:
             self._next = next
 
     def __init__(self):
+        """Initialize an empty stack."""
         pass
 
     def push(self, item):
+        """Push the given item on the stack."""
         pass
 
     def pop(self):
+        """Remove the last item that was pushed onto the stack."""
         pass
 
     def is_empty(self) -> bool:
+        """Return True if the stack contains no item."""
         pass
